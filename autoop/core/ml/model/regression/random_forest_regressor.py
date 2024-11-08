9dc829d2abd06b42adbc9f11fa906fe2e5d0e63a
mateivisan1 <m.visan@student.rug.nl>
2024-11-08 22:32:07 +0100

    added regression models

---
+++ autoop/core/ml/model/regression/random_forest_regressor.py
@@ -0,0 +1,16 @@
+import numpy as np
+from autoop.core.ml.model import Model
+from sklearn.ensemble import RandomForestRegressor
+
+
+class RandomForestRegressorModel(Model):
+    def __init__(self, parameters=None):
+        super().__init__(model_type="regression", parameters=parameters)
+        self.model = RandomForestRegressor(**(parameters or {}))
+
+    def train(self, X: np.ndarray, y: np.ndarray) -> None:
+        self.model.fit(X, y)
+        self.trained = True
+
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        return self.model.predict(X)
---
+++ autoop/core/ml/model/regression/ridge_regression.py
@@ -0,0 +1,16 @@
+import numpy as np
+from sklearn.linear_model import Ridge
+from autoop.core.ml.model import Model
+
+
+class RidgeRegressionModel(Model):
+    def __init__(self, parameters=None):
+        super().__init__(model_type="regression", parameters=parameters)
+        self.model = Ridge(**(parameters or {}))
+
+    def train(self, X: np.ndarray, y: np.ndarray) -> None:
+        self.model.fit(X, y)
+        self.trained = True
+
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        return self.model.predict(X)
