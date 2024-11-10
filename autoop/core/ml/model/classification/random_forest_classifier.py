c6e80fde742502fe6fb231dd2d889ab675d37bfd
mateivisan1 <m.visan@student.rug.nl>
2024-11-10 15:51:12 +0100

    updated version

--- autoop/core/ml/model/classification/random_forest_classifier.py
+++ autoop/core/ml/model/classification/random_forest_classifier.py
@@ -1,11 +1,17 @@
 import numpy as np
 from autoop.core.ml.model import Model
 from sklearn.ensemble import RandomForestClassifier
+from typing import Optional, Dict
 
 
 class RandomForestClassifierModel(Model):
-    def __init__(self, parameters=None):
-        super().__init__(model_type="classification", parameters=parameters)
+    def __init__(
+        self,
+        name: str,
+        asset_path: str,
+        parameters: Optional[Dict] = None
+    ):
+        super().__init__(model_type="classification", name=name, asset_path=asset_path, parameters=parameters)
         self.model = RandomForestClassifier(**(parameters or {}))
 
     def fit(self, X: np.ndarray, y: np.ndarray) -> None:
@@ -13,4 +19,6 @@ class RandomForestClassifierModel(Model):
         self.trained = True
 
     def predict(self, X: np.ndarray) -> np.ndarray:
+        if not self.trained:
+            raise ValueError("Model must be trained before making predictions.")
         return self.model.predict(X)
