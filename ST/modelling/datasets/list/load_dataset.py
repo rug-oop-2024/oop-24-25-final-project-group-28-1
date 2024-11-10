67fee15d815a5ca5cea3562fce43eb9b33c4a794
mateivisan1 <m.visan@student.rug.nl>
2024-11-10 23:41:41 +0100

    style check

--- ST/modelling/datasets/list/load_dataset.py
+++ ST/modelling/datasets/list/load_dataset.py
@@ -1,41 +1,36 @@
-from ST.core.system import AutoMLSystem
-import streamlit as st
-import pandas as pd
 import io
-
-#st.write("**Developers info**")
-#st.write("This is modelling/datasets/list/load_dataset.py")
-#st.write("\n\n Load existing datasets using the artifact registry. You can use a select box to achieve this.")
-#st.write("---")
+import pandas as pd
+import streamlit as st
+from ST.core.system import AutoMLSystem
 
 st.write("## Loading dataset")
 dataset = None
 if 'dataset_index' not in st.session_state:
     st.session_state.dataset_index = None
-     
-# An index for a dataset is not yet known, so we cannot use 
-# the helper function get_selected_dataset()   
+
+# An index for a dataset is not yet known, so we cannot use
+# the helper function get_selected_dataset()
 automl = AutoMLSystem.get_instance()
 datasets = automl.registry.list(type="dataset")
 
-selects = []
-for ds in datasets:
-    selects.append(ds.name)
- 
+selects = [ds.name for ds in datasets]
 
-prompt = "Select a data set:"    
-index = st.selectbox(prompt, range(len(selects)), format_func=lambda x: selects[x], index=None)
-if not index is None:
+prompt = "Select a data set:"
+index = st.selectbox(
+    prompt,
+    range(len(selects)),
+    format_func=lambda x: selects[x],
+    index=None
+)
+if index is not None:
     dataset = datasets[index]
-    data = dataset.data.decode()     # Should be using dataset.read() but this returns the bytes object only.
-                                     # TODO Find what must be done to be able to use the read method.
+    data = dataset.data.decode()  # Decoding bytes to string
+    # TODO: Find what must be done to be able to use the read method.
+
     retrieved_df = pd.read_csv(io.StringIO(data))
-    #retrieved_df = dataset.read()
-    #dataset = automl.registry.get(dataset.id)
     st.success(f"The selected dataset is {dataset.name}")
     st.write("### Properties:")
     st.write(retrieved_df)
     st.write("Features:", dataset.get_features())
-    
+
 st.session_state.dataset_index = str(index)
-    
